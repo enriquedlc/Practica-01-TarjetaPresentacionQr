9d046d5ee2234f02e1c4e4feaab51fbfca444d2a
Enrique <enriquedlc2d@gmail.com>
2022-10-02 18:02:01 +0100

    defined the main application structure

--- App.js
+++ App.js
@@ -4,17 +4,60 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.headerText}>photo and name</Text>
+      </View>
+      <View style={styles.description}>
+        <Text>description</Text>
+      </View>
+      <View style={styles.qrCode}>
+        <Text>qrCode</Text>
+      </View>
+
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: 'column',
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    flex: 1,
+    borderColor: 'pink',
+    borderWidth: 2,
+    width: '100%'
+  },
+
+  description: {
+    flex: 2,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'yellow',
+    borderWidth: 2,
+    width: '100%'
+  },
+
+  qrCode: {
+    flex: 2,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'black',
+    borderWidth: 2,
+    width: '100%'
+  },
+
+  headerText: {
+    marginTop: 60
+  }
 });
